386330e3b1d4e4427f40f1b625f3c4c32dd6398a
Ian <imcarthur2@gmail.com>
2019-12-03 11:16:23 -0500

    Print actual sensor values and percentage

--- arduino_sketches/sensor_read_light/sensor_read_light.ino
+++ arduino_sketches/sensor_read_light/sensor_read_light.ino
@@ -10,9 +10,12 @@ void setup() {
 void loop() {
     float reading = analogRead(LIGHTSENSORPIN); //Read light level
     float square_ratio = reading / 1023.0; //Get percent of maximum value (1023)
+    Serial.println(square_ratio);
     square_ratio = pow(square_ratio, 2.0); //Square to make response more obvious
     
     analogWrite(LEDPIN, 255.0 * square_ratio); //Adjust LED brightness relatively
+    Serial.println(reading);
     Serial.println(square_ratio); //Display reading in serial monitor
+    Serial.println("\n");
     delay(500);
 }
