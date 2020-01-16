c8b6bf1b7bd48f2e885aa8ea14774cafc42ce4cd
Ian <imcarthur2@gmail.com>
2020-01-16 10:37:52 -0500

    first committ of actuating library

---
+++ arduino_sketches/Rev0/main_test/actuator/actuator.cpp
@@ -0,0 +1,29 @@
+#include <actuator.h>
+#include "DHT.h"
+
+actuator::actuator(int num)
+{
+  pinMode(PUMP_PIN, OUTPUT);
+
+  pinMode(LED_PIN, OUTPUT);  
+}
+
+void actuator::enablePump()
+{
+  analogWrite(PUMP_PIN, 255); 
+}
+
+void actuator::disablePump()
+{
+  analogWrite(PUMP_PIN, 0);
+}
+
+void actuator::enableLED()
+{
+  analogWrite(LED_PIN, 255);
+}
+
+void actuator::disableLED()
+{
+    analogWrite(LED_PIN, 0);
+}
---
+++ arduino_sketches/Rev0/main_test/actuator/actuator.h
@@ -0,0 +1,18 @@
+#ifndef actuator_h
+#define actuator_h
+
+#define PUMP_PIN A2
+#define LED_PIN A3
+
+class actuator
+{
+  public:
+    actuator(int value); 
+    void enablePump();
+    void disablePump();
+    void enableLED();
+    void disableLED();
+};
+
+#endif
+
