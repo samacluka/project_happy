d2a80c247395edfb3cc3f8614bb3ad81b762e16a
Ian <imcarthur2@gmail.com>
2020-01-22 14:50:11 -0500

    water pumping logic in a thread!

--- arduino_sketches/Rev0/main_test/main_test.ino
+++ arduino_sketches/Rev0/main_test/main_test.ino
@@ -46,6 +46,9 @@ int is_water_present = 0;
 int light_value = 0;
 int pump_thread_active = 0;
 
+long int pump_start_time;
+
+
 sensor my_sensor(0);
 actuator my_actuator(0);
 
@@ -55,53 +58,51 @@ TimedAction pumpAction = TimedAction(500, pump);
 
 void setup()
 {
-    Serial.begin(9600);
+  Serial.begin(9600);
 
-    WiFiSetup();
-    rtcSetEpoch();
-    printrtc();
-    pinMode(LED_BUILTIN, OUTPUT);
+  //    WiFiSetup();
+  //    rtcSetEpoch();
+  //    printrtc();
+  pinMode(LED_BUILTIN, OUTPUT);
 
-    my_sensor.init();
+  my_sensor.init();
 }
 
 void loop()
 {
-    /* Sense System State */
+  /* Sense System State */
 
-    my_sensor.getTempHum(temperature, humidity);
-    my_sensor.getSoilMoist(dryness_of_soil);
-    my_sensor.getWaterLevel(is_water_present);
-    my_sensor.getHowMuchLight(light_value);
-
-    pumpAction.check();
-
-    Serial.print("temperature = ");
-    Serial.println(temperature, DEC);
-    Serial.print("humidity = ");
-    Serial.println(humidity, DEC);
-    Serial.print("soil moisture = ");
-    Serial.println(dryness_of_soil, DEC);
-    Serial.print("water present = ");
-    Serial.println(is_water_present, DEC);
-    Serial.print("light_value= ");
-    Serial.println(light_value, DEC);
+  my_sensor.getTempHum(temperature, humidity);
+  my_sensor.getSoilMoist(dryness_of_soil);
+  my_sensor.getWaterLevel(is_water_present);
+  my_sensor.getHowMuchLight(light_value);
 
 
-    /* Decide upon Actuation */
+  Serial.print("temperature = ");
+  Serial.println(temperature, DEC);
+  Serial.print("humidity = ");
+  Serial.println(humidity, DEC);
+  Serial.print("soil moisture = ");
+  Serial.println(dryness_of_soil, DEC);
+  Serial.print("water present = ");
+  Serial.println(is_water_present, DEC);
+  Serial.print("light_value= ");
+  Serial.println(light_value, DEC);
 
-    /* Pump Logic */
+
+  /* Decide upon Actuation */
+
+  /* Pump Logic */
+  pumpAction.check();
+
+  /* Light Logic */
+  if (light_value > MIN_LIGHT_THRESHOLD)
+  {
+
+  }
 
 
-
-    /* Light Logic */
-    if (light_value > MIN_LIGHT_THRESHOLD)
-    {
-
-    }
-
-
-    delay(5000);
+  delay(500);
 }
 
 
@@ -109,121 +110,121 @@ void loop()
 
 void rtcSetEpoch()
 {
-    unsigned long epoch;
-    int numberOfTries = 0, maxTries = 6;
-    do {
-        epoch = WiFi.getTime();
-        numberOfTries++;
-    }
-    while ((epoch == 0) && (numberOfTries < maxTries));
+  unsigned long epoch;
+  int numberOfTries = 0, maxTries = 6;
+  do {
+    epoch = WiFi.getTime();
+    numberOfTries++;
+  }
+  while ((epoch == 0) && (numberOfTries < maxTries));
 
-    if (numberOfTries == maxTries) {
-        Serial.print("NTP unreachable!!");
-        while (1);
-    }
-    else {
-        Serial.print("Epoch received: ");
-        Serial.println(epoch);
-        rtc.setEpoch(epoch);
+  if (numberOfTries == maxTries) {
+    Serial.print("NTP unreachable!!");
+    while (1);
+  }
+  else {
+    Serial.print("Epoch received: ");
+    Serial.println(epoch);
+    rtc.setEpoch(epoch);
 
-        Serial.println();
-    }
+    Serial.println();
+  }
+
+  // Print date...
+  Serial.print(rtc.getDay());
+  Serial.print("/");
+  Serial.print(rtc.getMonth());
+  Serial.print("/");
+  Serial.print(rtc.getYear());
+  Serial.print("\t");
+
+  // Print Time
+  Serial.print(rtc.getHours() + GMT);
+  Serial.print(":");
+  Serial.print(rtc.getMinutes());
+  Serial.print(":");
+  Serial.print(rtc.getSeconds());
+
+  Serial.println();
 }
 
 
 void printrtc()
 {
-    // Print date...
-    Serial.print(rtc.getDay());
-    Serial.print("/");
-    Serial.print(rtc.getMonth());
-    Serial.print("/");
-    Serial.print(rtc.getYear());
-    Serial.print("\t");
 
-    // Print Time
-    Serial.print(rtc.getHours() + GMT);
-    Serial.print(":");
-    Serial.print(rtc.getMinutes());
-    Serial.print(":");
-    Serial.print(rtc.getSeconds());
-
-    Serial.println();
 }
 /*-------------------------------------- Action --------------------------------------*/
 
 void pump()
 {
-    static long int pump_start_time;
+  //if the plant needs water and we have the water to do it
+  if ((dryness_of_soil > DRY_SOIL_MOISTURE - ((DRY_SOIL_MOISTURE - WET_SOIL_MOISTURE) / 3)) && is_water_present && (pump_thread_active == 0))
+  {
     
+    Serial.println("Soil is dry, water is present. Enabling pump.");
     
-    //if the plant needs water and we have the water to do it
-    if (dryness_of_soil > DRY_SOIL_MOISTURE - ((DRY_SOIL_MOISTURE - WET_SOIL_MOISTURE) / 3) && is_water_present && !pump_thread_active)
+    my_actuator.enablePump();
+    pump_thread_active = 1;
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    pump_start_time = millis();
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  }
+  else if (pump_thread_active)
+  {
+    my_sensor.getWaterLevel(is_water_present);
+    my_sensor.getSoilMoist(dryness_of_soil);
+    if (!is_water_present || !(dryness_of_soil > DRY_SOIL_MOISTURE - ((DRY_SOIL_MOISTURE - WET_SOIL_MOISTURE) / 3)))
     {
-        Serial.println("Soil is dry, water is present. Enabling pump.");
-
-        my_actuator.enablePump();
-        pump_thread_active = 1;
-        digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-        
-        digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+      my_actuator.disablePump();
+      pump_thread_active = 0;
     }
-    else if (pump_thread_active)
+    else if ( (millis() - pump_start_time) >  ALLOWED_PUMPING_TIME)
     {
-        my_sensor.getWaterLevel(is_water_present);
-        my_sensor.getSoilMoist(dryness_of_soil);
-        if (!is_water_present || !(dryness_of_soil > DRY_SOIL_MOISTURE - ((DRY_SOIL_MOISTURE - WET_SOIL_MOISTURE) / 3)))
-        {
-            my_actuator.disablePump();
-            pump_thread_active = 0;
-        }
-        else if ( (millis() - pump_start_time) >  ALLOWED_PUMPING_TIME)
-        {
-            my_actuator.disablePump();
-            pump_thread_active = 0;
-        }
-
+      my_actuator.disablePump();
+      pump_thread_active = 0;
     }
+
+  }
 
 }
 
 /*-------------------------------------- WEB COMM. --------------------------------------*/
 
 void WiFiSetup() {
-    WiFi.setPins(8, 7, 4, 2);
-    if (WiFi.status() == WL_NO_SHIELD) {
-        Serial.println("WiFi shield not present");
-        // don't continue:
-        while (true);
-    }
+  WiFi.setPins(8, 7, 4, 2);
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue:
+    while (true);
+  }
 
-    // attempt to connect to WiFi network:
-    while ( status != WL_CONNECTED) {
-        Serial.print("Attempting to connect to SSID: ");
-        Serial.println(ssid);
-        // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
-        status = WiFi.begin(ssid, pass);
+  // attempt to connect to WiFi network:
+  while ( status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
 
-        // wait 10 seconds for connection:
-        delay(10000);
-    }
-    // you're connected now, so print out the status:
-    printWiFiStatus();
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+  // you're connected now, so print out the status:
+  printWiFiStatus();
 }
 
 void printWiFiStatus() {
-    // print the SSID of the network you're attached to:
-    Serial.print("SSID: ");
-    Serial.println(WiFi.SSID());
-    // print your WiFi shield's IP address:
-    IPAddress ip = WiFi.localIP();
-    Serial.print("IP Address: ");
-    Serial.println(ip);
-    // print the received signal strength:
-    long rssi = WiFi.RSSI();
-    Serial.print("signal strength (RSSI):");
-    Serial.print(rssi);
-    Serial.println(" dBm");
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
 }
 
 /*
